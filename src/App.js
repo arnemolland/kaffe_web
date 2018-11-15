48ed1bfa10a26b8f6856d0f7a5255f86c24f3930
Arne Olai Molland <arneolaim@gmail.com>
2018-11-15 13:38:14 +0100

    :lipstick: CSS -> SASS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './images/kaffe_logo_black.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
